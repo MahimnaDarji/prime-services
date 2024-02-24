4c7ddc7c72892a32b8438c236c3d31c2204d91fb
Honey Patel <pathone@iu.edu>
2024-02-23 20:29:01 -0500

    add files

--- src/main/java/practicum6/primeservices/controller/PrimesController.java
+++ src/main/java/practicum6/primeservices/controller/PrimesController.java
@@ -1,4 +1,18 @@
 package practicum6.primeservices.controller;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
 public class PrimesController {
+    IPrimesService primesService;
+
+    public PrimesController(IPrimesService primesService) {
+        this.primesService = primesService;
+    }
+
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n) {
+        return primesService.isPrime(n);
+    }
 }
--- src/test/java/practicum6/primeservices/PrimeServicesApplicationTests.java
+++ src/test/java/practicum6/primeservices/PrimeServicesApplicationTests.java
@@ -4,10 +4,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PrimeServicesApplicationTests {
+class PrimesServiceTest {
+	PrimesService primesService = new PrimesService();
 
 	@Test
-	void contextLoads() {
+	void _45IsNotPrime() {
+		int n = 45;
+		boolean expected = false;
+		boolean actual = primesService.isPrime(n);
+		assertEquals(expected, actual);
 	}
 
-}
+	@Test
+	void _539828945930573IsNotPrime() {
+		Long n = 539828945930573L;
+		boolean expected = false;
+		boolean actual = primesService.isPrime(n);
+		assertEquals(expected, actual);
+	}
+
+
+	@Test
+	void _285191IsPrime() {
+		long n = 285191;
+		boolean expected = true;
+		boolean actual = primesService.isPrime(n);
+		assertEquals (expected, actual);
+	}
